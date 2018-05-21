8f2f97393e02b4c44e20156cdb38352aba4fb88d
John Paolo Paulino <paolo716@gmail.com>
2018-05-21 16:04:44 +0800

    Delete all item button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
     @trash_em = List.where(:completed => true).destroy_all
     redirect_to lists_url, notice: "All Markeed Item Were Successfully Deleted"
   end
+
+  def trash_all
+    @trash_all = List.all.destroy_all
+    redirect_to lists_url, notice: "All all Item Were Successfully Deleted"
+  end
 end
